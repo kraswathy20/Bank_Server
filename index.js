07a0a803012b4c603d35c7612aae1f3d3c1f7008
Aswathy k.r <aswathy19kr@gmail.com>
2023-03-22 23:53:39 +0530

    fourth commit

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 // import dataservice file
 const dataService=require('./service/dataservice')
+// import json web token
+const jwt=require("jsonwebtoken")
+// const jwt= require('jsonwebtoken')
 
 // import express
 
@@ -12,42 +15,90 @@ const app=express()
 // to parse json data from req body
 app.use(express.json())
 
+// middleware
+const jwtMiddleware=(req,res,next)=>{
+ try {const token=req.headers['access_token']
+    // verify token
+    const data=jwt.verify(token,"supersecretkey123")
+    console.log("----------middleware----------");
+    console.log(data);
+
+    next()
+}
+catch {
+    res.status(422).json({
+        statusCode:422,
+        status:false,
+        message:"please login first"
+    })
+
+
+}
+}
+
+// const jwtMiddleware=(req,res,next)=>{
+//     const token=req.body.token
+//     // verify token
+//      const data=jwt.verify(token,"supersecretkey123")
+//      console.log(data);
+//      next()
+// }
+
+
 // register  - post
 app.post('/register',(req,res)=>{
 
-   const result = dataService.register(req.body.uname,req.body.acno,req.body.psw)
-   res.status(result.statusCode).json(result)
-    // console.log(req.body);
-    // res.send("Success")
+    const result=dataService.register(req.body.uname,req.body.acno,req.body.psw)
+    // convert js to json and send
+    res.status(result.statusCode).json(result)
+    
 })
 
 // login
+
 app.get('/login',(req,res)=>{
 
-    const result = dataService.login(req.body.acno,req.body.psw)
+    const result=dataService.login(req.body.acno,req.body.psw)
+    // convert js to json and send
     res.status(result.statusCode).json(result)
-     // console.log(req.body);
-     // res.send("Success")
- })
+    
+})
 
 // Deposit
-app.post('/deposit',(req,res)=>{
+app.post('/deposit',jwtMiddleware,(req,res)=>{
 
-    const result = dataService.deposit(req.body.acnum,req.body.password,req.body.amount)
+    const result=dataService.deposit(req.body.acnum,req.body.password,req.body.amount)
+    // convert js to json and send
     res.status(result.statusCode).json(result)
-     // console.log(req.body);
-     // res.send("Success")
- })
+    
+})
+// app.post('/deposit',jwtMiddleware,(req,res)=>{
+
+//     const result = dataService.deposit(req.body.acnum,req.body.password,req.body.amount)
+//     res.status(result.statusCode).json(result)
+//      // console.log(req.body);
+//      // res.send("Success")
+//  })
 //  withdraw
-app.post('/withdraw',(req,res)=>{
+app.post('/withdraw',jwtMiddleware,(req,res)=>{
 
-    const result = dataService.withdraw(req.body.acnum,req.body.password,req.body.amount)
+    const result=dataService.withdraw(req.body.acnum,req.body.password,req.body.amount)
+    // convert js to json and send
     res.status(result.statusCode).json(result)
-     // console.log(req.body);
-     // res.send("Success")
- })
+    
+})
+
+// get transaction
+app.get('/transaction',jwtMiddleware,(req,res)=>{
+
+    const result=dataService.getTransaction(req.body.acno)
+    // convert js to json and send
+    res.status(result.statusCode).json(result)
+    
+})
 
 
+//  delete
 
 // create port
 app.listen(3000,()=>{console.log("server started at port number 3000");})
--- service/dataservice.js
+++ service/dataservice.js
@@ -1,3 +1,4 @@
+const jwt=require('jsonwebtoken')
 userDetails={
       1000:{acno:1000,username:"anu",password:"abc123",balance:0,transaction:[]},
       1001:{acno:1001,username:"amal",password:"abc123",balance:0,transaction:[]},
@@ -5,44 +6,83 @@ userDetails={
       1004:{acno:1004,username:"akhil",password:"abc123",balance:0,transaction:[]}
     }
 
-
-
-register=(uname,acno,psw)=>{
-    if(acno in userDetails){
-      return{
-        status:false,
-        message:"User already Exist",
-        statusCode:401
-      }
-    }
-    else{
-      userDetails[acno]={acno,username:uname,password:psw,balance:0,transaction:[]}
-      // console.log(this.userDetails);
-      
-      return {
-        status:true,
-        message:"Registered Successfully",
-        statusCode:200
-      }
-    }
-  
-  } 
-  
-  login=(acno, psw)=>{
-
+    register=(uname,acno,psw)=>{
       if(acno in userDetails){
-        if(psw==userDetails[acno]["password"]){
-          currentUser=userDetails[acno]["username"]
-          // alert("login Successfull!!")
-  
-          currentAcno=acno
+        return {
+          status:false,
+          message:"User already exist",
+          statusCode:401
+        }
+      }
+      else{
+        userDetails[acno]={acno,username:uname,password:psw,balance:0,transaction:[]}
+        // console.log(this.userDetails);
+        return {
+          status:true,
+          message:"Registered Successfully",
+          statusCode:200
+        }
+      }
+    
+    }
+
+
+    login=(acno,psw)=>{
+        if(acno in userDetails){
+          if(psw==userDetails[acno]["password"]){
+            currentUser=userDetails[acno]["username"]
+            // alert("login Successfull!!")
+    
+            currentAcno=acno
+
+            const token=jwt.sign({currentAcno},"supersecretkey123")
+
+            return {
+              status:true,
+              message:"Login Successfully",
+              statusCode:200,
+              currentUser,
+              currentAcno,
+              token
+            }
+          }
+          else{
+            return {
+              status:false,
+              message:"incorrect Password",
+              statusCode:401
+            }
+          }
+    
+        }
+        else{
+          return {
+            status:false,
+            message:"User not found.register please!!",
+            statusCode:401
+          }
+        }
         
+    }
+    
+    deposit=(acnum,password,amount)=>{
+      // convert string to integer
+      var amnt=parseInt(amount)
+      if(acnum in userDetails){
+        if(password==userDetails[acnum]["password"]){
+          // update balance
+          userDetails[acnum]["balance"]+=amnt
+    
+          // transaction data store
+          userDetails[acnum]["transaction"].push({Type:"CREDIT",amount:amnt})
+    
+    
+          // return current balance
+          // return userDetails[acnum]["balance"]
           return {
             status:true,
-            message:"Login Successfull",
-            statusCode:200,
-            currentAcno,
-            currentUser
+            message:`your account has been credited with amount ${amnt} and your current balance is ${userDetails[acnum]["balance"]}`,
+            statusCode:200
           }
         }
         else{
@@ -52,109 +92,74 @@ register=(uname,acno,psw)=>{
             statusCode:401
           }
         }
-  
       }
       else{
         return {
           status:false,
-          message:"User Not Found!!",
-          statusCode:401
-        }
-      }
-      
-  }
-
-  deposit=(acnum,password,amount)=>{
-   
-    // convert string to integer
-    var amnt=parseInt(amount)
-    if(acnum in userDetails){
-      if(password==userDetails[acnum]["password"]){
-        // update balance
-        userDetails[acnum]["balance"]+=amnt
-  
-        // transaction data store
-        userDetails[acnum]["transaction"].push({Type:"CREDIT",amount:amnt})
-  
-        
-  
-        // return current balance
-        // return userDetails[acnum]["balance"]
-        return {
-          status:true,
-          message:`${amnt} is creadited to your account and your balance is ${userDetails[acnum]["balance"]}`,
-          statusCode:200
-
-        }
-      }
-      else{
-        return {
-          status:false,
-          message:"Incorrect Password",
+          message:"Incorrect accnt number",
           statusCode:401
         }
       }
     }
-    else{
-      return {
-        status:false,
-        message:"Incorrect Account Number",
-        statusCode:401
-      }
-    }
-  }
 
-  withdraw=(acnum,password,amount)=>{
+
+    withdraw=(acnum,password,amount)=>{
+      // inorder to avoid calling this.userDetails
+      // convert string to integer
+      var amnt=parseInt(amount)
+      if(acnum in userDetails){
+        if(password==userDetails[acnum]["password"]){
+          if(amnt<=userDetails[acnum]["balance"]){
+            // update balance
+          userDetails[acnum]["balance"]-=amnt
     
-    // convert string to integer
-    var amnt=parseInt(amount)
-    if(acnum in userDetails){
-      if(password==userDetails[acnum]["password"]){
-        if(amnt<=userDetails[acnum]["balance"]){
-          // update balance
-        userDetails[acnum]["balance"]-=amnt
-  
-        userDetails[acnum]["transaction"].push({Type:"DEBIT",amount:amnt})
-          console.log(userDetails);
+          userDetails[acnum]["transaction"].push({Type:"DEBIT",amount:amnt})
+            console.log(userDetails);
+            
+            // return current balance
+          // return userDetails[acnum]["balance"]
+          return{
+            status:true,
+            message:`your account has been debited with amount ${amnt} 
+            and your current balance is ${userDetails[acnum]["balance"]}`,
+            statusCode:200
+          }
+          }
+          else{
+            return {
+              status:false,
+              message:"Insufficient Balance",
+              statusCode:401
+            }
+          }
           
-          
-          // return current balance
-        // return userDetails[acnum]["balance"]
-        return{
-          status:true,
-          message:`${amnt} is withdrawn from your account and your current balance is ${userDetails[acnum]["balance"]}`,
-          statusCode:200
-
-        }
         }
         else{
-          
           return {
             status:false,
-            message:"Insufficient balance",
+            message:"Incorrect password",
             statusCode:401
           }
         }
-        
       }
       else{
-        
         return {
           status:false,
-          message:"Incorrect password",
+          message:"Incorrect Accnt Number",
           statusCode:401
         }
       }
     }
-    else{
-      
-      return {
-        status:false,
-        message:"Incorrect acnum",
-        statusCode:401
+
+    getTransaction=(acno)=>{
+      // return userDetails[acno]["transaction"]
+      return{
+        status:true,
+        statusCode:200,
+        transaction:userDetails[acno]["transaction"]
       }
     }
-  }
+
   module.exports={
-    register,login,deposit,withdraw
+    register,login,deposit,withdraw,getTransaction
   }
